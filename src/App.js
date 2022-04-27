080c9df7c887417e3a7a1ae349d849ea57012506
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2022-04-27 17:35:17 +0900

    "proxy 220427"

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
 // routes
 import Router from './routes';
 // theme
@@ -8,12 +11,44 @@ import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 
 // ----------------------------------------------------------------------
 
-export default function App() {
+
+function App() {
+
+  const [hello, setHello] = useState('')
+
+  useEffect(() => {
+      axios.get('/api/hello')
+      .then(response => setHello(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
     <ThemeProvider>
+              <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
       <ScrollToTop />
       <BaseOptionChartStyle />
       <Router />
     </ThemeProvider>
   );
 }
+
+
+// function App() {
+//    const [hello, setHello] = useState('')
+
+//     useEffect(() => {
+//         axios.get('/api/hello')
+//         .then(response => setHello(response.data))
+//         .catch(error => console.log(error))
+//     }, []);
+
+//     return (
+//         <div>
+//             백엔드에서 가져온 데이터입니다 : {hello}
+//         </div>
+//     );
+// }
+
+export default App;
