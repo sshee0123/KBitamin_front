ff27b25b7d0b388f1c946b231d8e7d7db78e3f0b
BaekJeongEun <sophia991106@gmail.com>
2022-05-11 10:27:23 +0900

    220511 User_taking request

--- src/service/MedicineService.js
+++ src/service/MedicineService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 const MEDICINE_API_BASE_URL = "/api/medicine"; 
+const TAKING_API_BASE_URL = "/api/taking"; 
 
 class MedicineService{
 
@@ -24,6 +25,12 @@ class MedicineService{
         return axios.get(`${MEDICINE_API_BASE_URL}/detailOneMediInfo?name=${name}`,{ headers: authHeader() });
     }
 
+    // eslint-disable-next-line class-methods-use-this
+    getTakingPerUser(id){
+        console.log("front service getTakingPerUser?user_id=` + user_id,",id);
+
+        return axios.get(`${TAKING_API_BASE_URL}/takingUser?id=${id}`,{ headers: authHeader() });
+    }
 
 }
 
